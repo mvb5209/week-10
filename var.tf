a8b586dc1a134546a4a1abcff1b4ebd1311e1f94
MannyB <mannyb@mannybs-mbp.mynetworksettings.com>
2024-11-14 11:45:56 -0500

    jenk

--- var.tf
+++ var.tf
@@ -1,9 +1,27 @@
+# variable "ENVIRONMENT" {
+  
+# }
+# variable "INSTANCE_TYPE" {
+  
+# }
+# variable "REGION" {
+  
+# }
+
 variable "ENVIRONMENT" {
-  
+  description = "The environment for the deployment (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"  # You can set a default or leave it empty
 }
+
 variable "INSTANCE_TYPE" {
-  
+  description = "The type of EC2 instance to use (e.g., t2.micro, m5.large)"
+  type        = string
+  default     = "t2.micro"
 }
+
 variable "REGION" {
-  
+  description = "The AWS region to deploy the resources to (e.g., us-east-1, us-west-2)"
+  type        = string
+  default     = "us-east-1"
 }
